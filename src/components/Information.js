ce445551d7fe862c4bfa2aaeb0b7bfffabf526e8
junhyoung2 <jun83467@gmail.com>
2025-08-01 12:48:01 +0900

    0801 12:47 merge

--- src/components/Information.js
+++ src/components/Information.js
@@ -1,9 +1,39 @@
 import Header from "./Header";
 import Navigate from "../components/Navigate";
 import { useNavigate } from "react-router-dom";
+import { useState ,useEffect } from "react";
+import { getMyPasses } from "../utils/towerpickapi";
 
 const Information = () => {
     const navigate = useNavigate();
+
+    //로그인 상태 확인
+    const [loading, setLoading] = useState(true);
+
+    //정기권 보유시 구매 창으로 넘어가기 x
+    const [seasonTicket, setSeasonTicket] = useState(false);
+
+    //현재 로그인 한 사용자 ID
+    const towerpickData = localStorage.getItem("towerpick");
+    const userID = towerpickData ? JSON.parse(towerpickData).member_id : null;
+
+    useEffect(() => {
+        const fetchData = async () => {
+            //정기권 보유 여부 확인
+            const { data: passData, error: passError } = await getMyPasses(userID);
+            if (passError) {
+                console.error("정기권 정보를 불러오지 못 했습니다")
+            } else {
+                const activePass = passData?.find(pass => pass.status === "active" && pass.is_paid);
+                setSeasonTicket(!!activePass);
+            }
+
+            setLoading(false);
+
+        };
+        fetchData();
+    }, []);
+
     return (
         <div>
             {/* 공통헤더 */}
@@ -35,7 +65,18 @@ const Information = () => {
                         </p>
                         <button
                             onClick={() => {
-                                navigate("/season1");
+                                if (loading) {
+                                    alert("정보를 불러오는 중입니다")
+                                    return;
+                                }
+
+                                console.log("seasonTicket at click", seasonTicket);
+
+                                if (seasonTicket) {
+                                    alert("사용중인 정기권이 존재합니다");
+                                } else {
+                                    navigate("/season1");
+                                }
                             }}
                         >
                             바로가기
